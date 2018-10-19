2e004f6bf0def31cfc769467ba5179d9b585ecea
Jordan Rand <jorrand@gmail.com>
2018-10-19 10:59:08 -0600

    init

---
+++ reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+import currentGame from './currentGame'
+import user from './user'
+
+const rootReducer = combineReducers({
+  currentGame,
+  user,
+})
+
+export default rootReducer
+
